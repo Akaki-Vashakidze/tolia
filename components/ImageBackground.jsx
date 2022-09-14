bb79129016a3f41c0d90d63b69bb6999ec1c3f94
Akaki <Akaki@Pro1.local>
2022-09-14 23:24:36 +0400

    new

--- components/ImageBackground.jsx
+++ components/ImageBackground.jsx
@@ -1,35 +1,52 @@
-import React, { Fragment, useRef ,useEffect, useState } from "react";
+import React, { Fragment, useRef, useEffect, useState } from "react";
 import Image from "next/image";
+import { BiPhoneCall , BiMapPin} from "react-icons/bi";
+import { GrMapLocation } from "react-icons/gr";
 
 const ImageBackground = () => {
-  let width = 400;
-  let height = 400;
   let n = 1;
-  const effectRan = useRef('false') 
+  const effectRan = useRef("false");
 
   const [link, setLink] = useState("/toliaImages/pool1.jpeg");
 
   useEffect(() => {
-    if (effectRan.current === 'false') {
+    if (effectRan.current === "false") {
       setInterval(() => {
         setLink("/toliaImages/pool" + n + ".jpeg");
-        n < 4 ? n++ : n = 1;
+        n < 4 ? n++ : (n = 1);
         console.log(n);
       }, 3000);
     }
 
     return () => {
-      effectRan.current = 'true';
+      effectRan.current = "true";
     };
   }, []);
 
   return (
     <Fragment>
       <div className="top-0 left-0 w-full h-full fixed">
-        <Image src={link} alt="/" layout="fill"/>
+        <Image src={link} alt="/" layout="fill" />
       </div>
       <div className="top-o w-full left-0 fixed h-[100%] md:left-[10%] bg-gray-900 md:w-[80%] opacity-50 "></div>
-</Fragment>
+      <div className="text-white fixed top-[95%] h-[5%] w-full left-0 bg-neutral-900 opacity-95 flex justify-center">
+        <div className="w-[70%] justify-between items-center flex">
+          <span className="flex justify-between items-center">
+            <span className="mr-2">
+              <BiMapPin />
+            </span>
+            <span>მისამართი: რუსთავი, მშენებელთა ქ. 15</span>
+          </span>
+
+          <span className="flex justify-between items-center">
+            <span className="mr-2">
+              <BiPhoneCall />
+            </span>
+            <span>საკონტაქტო ნომ: 0341 25 71 00 </span>
+          </span>
+        </div>
+      </div>
+    </Fragment>
   );
 };
 
