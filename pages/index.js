96f196927ecdd4741813ff4e5355b302963d5aca
Akaki <Akaki@Pro1.local>
2022-09-14 22:35:51 +0400

    erroe1

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Fragment } from 'react'
 import NavBar from '../components/NavBar';
-import ImageBackground from '../components/imageBackground';
+import ImageBackground from '../components/ImageBackground';
 
  const Home = () => {
   return (
